f475fcf56e1eed8013d5895c04e0e2eafc3b1fd1
Alex Roeum <avroeum@gmail.com>
2015-10-04 09:45:38 -0400

    ProjectShards

---
+++ Platformer2D/Platforms/WindowsDX/Game/Hitbox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Platformer2D;
+
+namespace ShardLands.Game
+{
+    class Hitbox
+    {
+        Vector2 hitLocation;
+        bool hit;
+
+        public Hitbox(Player player)
+        {
+            hitLocation = player.Position;
+            hit = false;
+        }
+        
+        void runHit()
+        {
+            
+        }
+
+        void checkHit(Enemy enemy)
+        {
+            if (hitLocation.X == enemy.Position.X)
+            {
+                hit = true;
+                
+            }
+        }
+    }
+}
